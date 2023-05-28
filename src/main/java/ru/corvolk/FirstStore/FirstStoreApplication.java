721aa8508c4c0b12567e41c1bca5e75be47b8c91
AndreyLk <corvolk@yandex.ru>
2023-05-28 11:53:24 +0300

    Init project

---
+++ src/main/java/ru/corvolk/FirstStore/FirstStoreApplication.java
@@ -0,0 +1,13 @@
+package ru.corvolk.FirstStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/corvolk/FirstStore/FirstStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.corvolk.FirstStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
